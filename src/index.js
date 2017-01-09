6a35c8d2929ebb1f16078256ac54ddf813203d3d
knight42 <anonymousknight96@gmail.com>
2017-01-09 22:17:27 +0800

    Not to apply polyfill in index.js

--- src/index.js
+++ src/index.js
@@ -2,63 +2,40 @@
 
 'use strict'
 
-var port = process.env.PORT || 9999
+const config = require('./config')
+const port = config.apiport
 
-function applyPolyFill() {
-  require('babel-register')({
-    'presets': [
-      'es2015',
-      'stage-1'
-    ],
-  })
-  require('babel-polyfill')
-}
-
-if (!process.env.NODE_ENV) {
-  process.env.NODE_ENV = 'production'
-}
-
-if (process.env.NODE_ENV === 'test') {
-  applyPolyFill()
+if (config.isTest) {
   module.exports = require('./app.js').listen(port).address()
 } else {
-  var basename = require('path').basename
-  var progName = basename(process.argv[1])
-
-  // process.argv[0] is /usr/bin/node
-  switch (progName) {
-    case 'yukid':
-      applyPolyFill()
-      var server = require('./app.js').listen(port, function() {
-        var addr = server.address()
+  const action = process.argv[2]
+  switch (action) {
+    case 'daemon': {
+      const server = require('./app.js').listen(port, () => {
+        const addr = server.address()
         console.log('listening on', addr.address + ':' + addr.port)
       })
+    }
       break
-
-    case 'yuki':
-      var action = process.argv[2]
-      var repo = process.argv[3]
-      var request = require('./request')
-      switch (action) {
-        case 'sync':
-          request('http://localhost:' + port + '/api/v1/repositories/' + repo + '/sync')
-            .then(function(res) {
-              if (res.ok) {
-                console.log('syncing ' + repo)
-              } else {
-                throw new Error('Server return ' + res.status)
-              }
-            })
-            .catch(console.error)
-          break;
-
-        default:
-          console.error('Unknown action: ' + action)
+    case 'sync': {
+      const request = require('./request')
+      const repo = process.argv[3]
+      if (typeof repo === 'undefined') {
+        console.error('Repo name is needed')
+        process.exit(1)
       }
-      break
-
+      request(`http://127.0.0.1:${port}/api/v1/repositories/${repo}/sync`)
+        .then((res) => {
+          if (res.ok) {
+            console.log('syncing ' + repo)
+          } else {
+            throw new Error('Server return ' + res.status)
+          }
+        })
+        .catch(console.error)
+    }
+      break;
     default:
-      console.error('Unknown program: ' + progName)
+      console.error('Unknown action: ' + action)
   }
 }
-
