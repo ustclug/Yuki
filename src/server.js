9b2a211de0d539c130157e239c395398a5e02dfa
knight42 <anonymousknight96@gmail.com>
2017-01-17 23:50:48 +0800

    server: remove exited containers in prod env

--- src/server.js
+++ src/server.js
@@ -4,7 +4,9 @@
 
 import Koa from 'koa'
 import routes from './routes'
-import serve from 'koa-static'
+import mongoose from 'mongoose'
+import docker from './docker'
+import config from './config'
 
 const app = new Koa()
 
@@ -12,5 +14,38 @@ app.use(routes)
 app.on('error', (err) => {
   console.error('Uncaught error: ', err)
 })
+
+const uri = config.isTest ?
+  `mongodb://${config.DB_HOST}/test` :
+  `mongodb://${config.DB_USER}:${config.DB_PASSWD}@\
+${config.DB_HOST}:${config.DB_PORT}/${config.DB_NAME}`
+mongoose.Promise = Promise
+
+mongoose.connect(uri, {
+  promiseLibrary: Promise,
+})
+
+// cleanup
+if (config.isProd) {
+  docker.listContainers({
+    all: true,
+    filters: {
+      label: {
+        syncing: true
+      },
+      status: {
+        exited: true
+      }
+    }
+  })
+  .then(cts => {
+    console.log('Cleaning exited containers')
+    cts.forEach(info => {
+      const ct = docker.getContainer(info.Id)
+      ct.remove({ v: true })
+        .catch(console.error)
+    })
+  })
+}
 
 module.exports = app
