c91fba1129bd001f9e6fb016abf976db791fc7de
knight42 <anonymousknight96@gmail.com>
2017-01-18 11:17:48 +0800

    Simplify index.js

--- src/index.js
+++ src/index.js
@@ -3,24 +3,9 @@
 'use strict'
 
 const config = require('./config')
-const port = config.API_PORT
 
-if (config.isTest) {
-  require('./server.js').listen(port)
+if (config.isTest || process.argv[2] === 'daemon') {
+  require('./server')
 } else {
-  const action = process.argv[2]
-  switch (action) {
-    case 'daemon': {
-      const server = require('./server.js').listen(port, () => {
-        const addr = server.address()
-        console.log('listening on', addr.address + ':' + addr.port)
-      })
-    }
-      break
-    case 'sync':
-      require('./client.js')
-      break;
-    default:
-      console.error('Unknown action: ' + action)
-  }
+  require('./client')
 }
