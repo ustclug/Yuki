842754978707791b4aaa76391da062f09ce4b020
knight42 <anonymousknight96@gmail.com>
2017-01-26 02:44:06 +0800

    request: support options from user

--- src/request.js
+++ src/request.js
@@ -5,14 +5,16 @@
 var fetch = require('node-fetch')
 fetch.Promise = require('bluebird')
 
-module.exports = function request(url, data, method) {
-  method = method || 'GET'
-  var opts = {}
+module.exports = function request(url, data, method = 'GET', useropts = {}) {
+  var opts = {
+    headers: {
+      'User-Agent': 'Nagato Yuki/1.0'
+    }
+  }
+  Object.assign(opts, useropts)
   if (data !== null && typeof data === 'object') {
     if (method === 'GET') method = 'POST'
-    opts.headers = {
-      'Content-Type': 'application/json'
-    }
+    opts.headers['Content-Type'] = 'application/json'
     opts.body = JSON.stringify(data)
   }
   opts.method = method
