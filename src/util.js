9a9e9eddf1863ca06c97a3cfa110daa938d6cee4
knight42 <anonymousknight96@gmail.com>
2017-02-02 17:21:33 +0800

    util: remove unused function

--- src/util.js
+++ src/util.js
@@ -7,44 +7,10 @@ import path from 'path'
 import docker from './docker'
 import models from './models'
 import CONFIG from './config'
-import readline from 'readline'
-import {Transform} from 'stream'
 
 const Repo = models.Repository
 const PREFIX = CONFIG.CT_NAME_PREFIX
 const LABEL = CONFIG.CT_LABEL
-
-// All Transform streams are also Duplex Streams
-function progresBar(stream) {
-  return new Transform({
-    writableObjectMode: true,
-
-    transform(chunk, _, callback) {
-      const data = JSON.parse(chunk.toString());
-
-      // Sample data:
-      // {"status":"Pulling from library/centos","id":"latest"}
-      // {"status":"Pulling fs layer","progressDetail":{},"id":"8d30e94188e7"}
-      // {"status":"Downloading","progressDetail":{"current":531313,"total":70591526},"progress":"[=> ] 531.3 kB/70.59 MB","id":"8d30e94188e7"} ]"
-      // {"status":"Verifying Checksum","progressDetail":{},"id":"8d30e94188e7"}
-      // {"status":"Download complete","progressDetail":{},"id":"8d30e94188e7"}
-      // {"status":"Extracting","progressDetail":{"current":557056,"total":70591526},"progress":"[=> ] 557.1 kB/70.59 MB","id":"8d30e94188e7"} ]"
-      // {"status":"Pull complete","progressDetail":{},"id":"8d30e94188e7"}
-      // {"status":"Digest: sha256:2ae0d2c881c7123870114fb9cc7afabd1e31f9888dac8286884f6cf59373ed9b"}
-      // {"status":"Status: Downloaded newer image for centos:latest"}
-      //
-
-      //stream.write(chunk)
-      stream.write('\r')
-      readline.clearLine(stream, 1) // clear to the right
-      stream.write(data.status)
-      if (data.hasOwnProperty('progress')) {
-        stream.write(': ' + data.progress)
-      }
-      callback()
-    }
-  }).setEncoding('utf8')
-}
 
 async function bringUp(cfg) {
   let ct
@@ -94,7 +60,7 @@ async function queryOpts({ name, debug = false }) {
     },
     name: `${PREFIX}-${name}`,
   }
-  opts.Env.push(`REPO=${name}`, `BIND_ADDRESS=${CONFIG.BIND_ADDR}`)
+  opts.Env.push(`REPO=${name}`, `BIND_ADDRESS=${CONFIG.BIND_ADDRESS}`)
   if (debug) {
     opts.Env.push('DEBUG=true')
   }
