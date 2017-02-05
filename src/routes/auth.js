96fab89c7f8cdac1809cd9a03496d0f1b438f44e
knight42 <anonymousknight96@gmail.com>
2017-02-05 11:40:42 +0800

    auth: username can be set in tests

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,14 +6,10 @@ import { isTest, TOKEN_NAME } from '../config'
 import { User } from '../models'
 
 export default async function(ctx, next) {
-  if (isTest) {
-    ctx.state.authorized = true
-    return next()
-  }
   const token = ctx.header[TOKEN_NAME] || ''
   const user = await User.findOne({ token })
   if (user === null) {
-    ctx.state.authorized = false
+    ctx.state.authorized = isTest ? true : false
   } else {
     ctx.state.authorized = true
     ctx.state.isAdmin = user.admin
