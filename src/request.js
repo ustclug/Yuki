6173c9a52d517c7e7609ffce657c243d259d2399
knight42 <anonymousknight96@gmail.com>
2017-02-05 11:40:16 +0800

    req: set default ua

--- src/request.js
+++ src/request.js
@@ -5,13 +5,14 @@
 var fetch = require('node-fetch')
 fetch.Promise = require('bluebird')
 
-module.exports = function request(url, data, method = 'GET', useropts = {}) {
-  var opts = {
-    headers: {
-      'User-Agent': 'Nagato Yuki/1.0'
-    }
+module.exports = function request(url, data, method = 'GET', opts = {}) {
+  const ua = 'Nagato Yuki/1.0'
+  opts.headers = opts.headers || {
+    'User-Agent': ua,
   }
-  Object.assign(opts, useropts)
+  if (!opts.headers['User-Agent']) {
+    opts.headers['User-Agent'] = ua
+  }
   if (data !== null && typeof data === 'object') {
     if (method === 'GET') method = 'POST'
     opts.headers['Content-Type'] = 'application/json'
