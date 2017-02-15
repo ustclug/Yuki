611d99bd9cd884a0ab47cf403af1a09884bd246d
knight42 <anonymousknight96@gmail.com>
2017-02-15 19:26:20 +0800

    config: refactor

--- src/config.js
+++ src/config.js
@@ -6,6 +6,8 @@ if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = 'production'
 }
 
+const path = require('path')
+
 function merge(target, src, prefix = '') {
   Object.keys(target).forEach(k => {
     if (typeof src[`${prefix}${k}`] !== 'undefined')
@@ -13,70 +15,89 @@ function merge(target, src, prefix = '') {
   })
 }
 
-const defaultCfg = {
-  // For server
-  DB_USER: '',
-  DB_PASSWD: '',
-  DB_HOST: '127.0.0.1',
-  DB_NAME: 'mirror',
-  DB_PORT: 27017,
-  API_PORT: 9999,
-  DOCKERD_PORT: 2375,
-  DOCKERD_HOST: '127.0.0.1',
-  DOCKERD_SOCKET: '/var/run/docker.sock',
-  BIND_ADDRESS: '',
-  CT_LABEL: 'syncing',
-  CT_NAME_PREFIX: 'syncing',
-  LOGDIR_ROOT: '/var/log/ustcmirror',
-  IMAGES_UPGRADE_INTERVAL: '1 * * * *',
-  OWNER: `${process.getuid()}:${process.getgid()}`,
+const isDev = process.env.NODE_ENV.startsWith('dev')
+const isProd = process.env.NODE_ENV.startsWith('prod')
+const isTest = process.env.NODE_ENV.startsWith('test')
 
-  // For client
-  API_ROOT: '',
-}
-
-defaultCfg.API_ROOT = `http://localhost:${defaultCfg.API_PORT}/`
-const path = require('path')
-const fps = ['/etc/ustcmirror/config', path.join(process.env['HOME'], '.ustcmirror/config')]
-
-for (const fp of fps) {
-  let cfg
-  try {
-    cfg = require(fp)
-  } catch (e) {
-    if (e.code !== 'MODULE_NOT_FOUND') {
-      throw e
-    }
-    continue
+let defaults = null
+const reload = () => {
+  defaults = {
+    // For server
+    DB_USER: '',
+    DB_PASSWD: '',
+    DB_HOST: '127.0.0.1',
+    DB_NAME: 'mirror',
+    DB_PORT: 27017,
+    API_PORT: 9999,
+    DOCKERD_PORT: 2375,
+    DOCKERD_HOST: '127.0.0.1',
+    DOCKERD_SOCKET: '/var/run/docker.sock',
+    BIND_ADDRESS: '',
+    CT_LABEL: 'syncing',
+    CT_NAME_PREFIX: 'syncing',
+    LOGDIR_ROOT: '/var/log/ustcmirror',
+    IMAGES_UPGRADE_INTERVAL: '1 * * * *',
+    OWNER: `${process.getuid()}:${process.getgid()}`,
+    LOGLEVEL: '',
+    // For client
+    API_ROOT: '',
   }
-  merge(defaultCfg, cfg)
+  defaults.API_ROOT = `http://localhost:${defaults.API_PORT}/`
+
+  defaults.isDev = isDev
+  defaults.isTest = isTest
+  defaults.isProd = isProd
+
+  // should be lower case
+  defaults['TOKEN_NAME'] = 'x-mirror-token'
+
+  defaults._images = [
+    'ustcmirror/gitsync:latest',
+    'ustcmirror/rsync:latest',
+    'ustcmirror/lftpsync:latest',
+  ]
+
+  const fps = ['/etc/ustcmirror/config', path.join(process.env['HOME'], '.ustcmirror/config')]
+
+  for (const fp of fps) {
+    let cfg
+    try {
+      cfg = require(fp)
+    } catch (e) {
+      if (e.code !== 'MODULE_NOT_FOUND') {
+        throw e
+      }
+      continue
+    }
+    merge(defaults, cfg)
+  }
+
+  merge(defaults, process.env, 'YUKI_')
+
+  if (!defaults.LOGLEVEL) {
+    defaults.LOGLEVEL = isDev ? 'debug' : 'warn'
+  }
+
+  if (!isTest) {
+    if (!(process.argv[2] !== 'daemon' ||
+      defaults.BIND_ADDRESS))
+    {
+      throw new Error('Need to specify <BIND_ADDRESS> in configuration')
+    }
+
+    if (!/(error|warn|info|verbose|debug|silly)/.test(defaults.LOGLEVEL))
+    {
+      throw new Error(`Invalid LOGLEVEL: ${defaults.LOGLEVEL}`)
+    }
+  }
+
+  if (isDev) {
+    console.log('Configuration:', JSON.stringify(defaults, null, 4))
+  }
+
+  defaults.reload = reload
 }
 
-merge(defaultCfg, process.env, 'YUKI_')
+reload()
 
-defaultCfg.isDev = process.env.NODE_ENV.startsWith('dev')
-defaultCfg.isProd = process.env.NODE_ENV.startsWith('prod')
-defaultCfg.isTest = process.env.NODE_ENV.startsWith('test')
-
-if (!(defaultCfg.isTest ||
-    process.argv[2] !== 'daemon' ||
-    defaultCfg.BIND_ADDRESS))
-{
-  console.error('Need to specify <BIND_ADDRESS> in configuration')
-  process.exit(1)
-}
-
-// should be lower case
-defaultCfg['TOKEN_NAME'] = 'x-mirror-token'
-
-defaultCfg._images = [
-  'ustcmirror/gitsync:latest',
-  'ustcmirror/rsync:latest',
-  'ustcmirror/lftpsync:latest',
-]
-
-if (defaultCfg.isDev) {
-  console.log('Configuration:', JSON.stringify(defaultCfg, null, 4))
-}
-
-module.exports = defaultCfg
+export default defaults
