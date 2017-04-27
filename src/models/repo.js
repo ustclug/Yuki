70c9431597255d496b123961fc37f47ea1901892
knight42 <anonymousknight96@gmail.com>
2017-04-27 15:10:29 +0800

    models::Repo: move virtual getter of `upstream` to models::Meta

--- src/models/repo.js
+++ src/models/repo.js
@@ -35,48 +35,11 @@ const schema = new mongoose.Schema({
   },
   bindIp: String,
   user: String,
-}, { id: false, strict: 'throw' })
-
-function isUndef(val) {
-  return typeof val === 'undefined'
-}
-
-schema.virtual('upstream')
-  .get(function() {
-    const defVal = 'unknown'
-    if (isUndef(this.image) || isUndef(this.envs)) {
-      return
-    }
-    if (!this.image.startsWith('ustcmirror/')) {
-      return defVal
-    }
-    const img = this.image.splitN('/', 1)[1].splitN(':', 1)[0]
-    switch (img) {
-      case 'rsync':
-      case 'archvsync': {
-        const host = this.envs.RSYNC_HOST || '(unknown)'
-        const path = this.envs.RSYNC_PATH || '(unknown)'
-        return `rsync://${host}/${path}`
-      }
-      case 'lftpsync': {
-        const host = this.envs.LFTPSYNC_HOST || '(unknown)'
-        const path = this.envs.LFTPSYNC_PATH || '(unknown)'
-        return `${host}/${path}`
-      }
-      case 'gitsync':
-        return this.envs.GITSYNC_URL
-      case 'aptsync':
-        return this.envs.APTSYNC_URL
-      case 'pypi':
-        return this.envs.PYPI_MASTER || 'https://pypi.python.org'
-      case 'homebrew-bottles':
-        return this.envs.HOMEBREW_BOTTLE_DOMAIN || 'http://homebrew.bintray.com'
-      case 'rubygems':
-        return this.envs.UPSTREAM || 'http://rubygems.org'
-      default:
-        return defVal
-    }
-  })
+}, {
+  id: false,
+  strict: 'throw',
+  timestamps: true
+})
 
 schema.virtual('name')
   .set(function(name) {
