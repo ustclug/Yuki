4e2434c8f44a22d30e8a06785b1f3bcb42bc72e4
knight42 <anonymousknight96@gmail.com>
2017-04-27 15:09:51 +0800

    models::Log: alter fields

--- src/models/log.js
+++ src/models/log.js
@@ -5,27 +5,19 @@
 import mongoose from 'mongoose'
 
 const schema = new mongoose.Schema({
-  _id: String,
-  lastSuccess: Date,
+  name: {
+    type: String,
+    required: true
+  },
   exitCode: {
     type: Number,
     default: -1
-  },
-  status: {
-    enum: ['success', 'failure', 'running', 'unknown'],
-    default: 'unknown',
-    type: String
   },
 }, {
   id: false,
   strict: 'throw',
   timestamps: true
 })
-
-schema.virtual('name')
-  .set(function(name) {
-    this._id = name
-  })
 
 schema.set('toJSON', { versionKey: false, getters: true })
 export default mongoose.model('Log', schema)
