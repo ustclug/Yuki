1652bd6d6a67d951e76c9bceab14b3f1fab61ebf
knight42 <anonymousknight96@gmail.com>
2017-05-18 22:15:47 +0800

    logger: write logs to file
    Close #4

--- packages/yukid/src/logger.js
+++ packages/yukid/src/logger.js
@@ -3,20 +3,29 @@
 'use strict'
 
 import winston from 'winston'
-import { LOGLEVEL, TIMESTAMP } from './config'
+import { LOGLEVEL, TIMESTAMP, LOGDIR_ROOT } from './config'
+import { IS_TEST } from './globals'
 import moment from 'moment'
 
 const getLocalTime = () => moment().local().format('YYYY-MM-DD HH:mm:ss')
 
-export default new (winston.Logger)({
-  transports: [
-    new (winston.transports.Console)({
-      level: LOGLEVEL,
-      formatter: (options) => {
-        return TIMESTAMP ?
-          `[${getLocalTime()}] ${options.level.toUpperCase()}: ${options.message}` :
-          `${options.level.toUpperCase()}: ${options.message}`
-      }
-    })
-  ]
-})
+const transports = []
+
+if (!IS_TEST) {
+  transports.push(new (winston.transports.File)({
+    level: LOGLEVEL,
+    json: false,
+    filename: `${LOGDIR_ROOT}/yukid.log`,
+    maxsize: 1024 * 1024 * 10,
+    maxFiles: 30,
+    formatter: (options) => {
+      return TIMESTAMP ?
+        `[${getLocalTime()}] ${options.level.toUpperCase()}: ${options.message}` :
+        `${options.level.toUpperCase()}: ${options.message}`
+    }
+  }))
+} else {
+  transports.push(new (winston.transports.Console)({ level: LOGLEVEL }))
+}
+
+export default new (winston.Logger)({ transports })
