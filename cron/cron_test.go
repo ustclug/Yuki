0890f2636bf63fe7f89159ea3b6e69ad97bf118e
knight42 <anonymousknight96@gmail.com>
2018-02-15 00:04:05 +0800

    chore: update deps

--- cron/cron_test.go
+++ cron/cron_test.go
@@ -2,21 +2,22 @@ package cron
 
 import (
 	"sync"
-	"time"
 	"testing"
+	"time"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestCron(t *testing.T) {
 	var wg sync.WaitGroup
 	now := time.Now()
+	next := now.Add(time.Second * 4)
 	wg.Add(1)
 	cron := New()
 	cron.AddFunc("@every 3s", func() {
-		defer wg.Done()
+		wg.Done()
 	})
-	cron.Start()
 	wg.Wait()
 	now2 := time.Now()
-	assert.True(t, now2.After(now))
+	assert.True(t, now2.After(now) && now2.Before(next))
 }
