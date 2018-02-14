6f3444b3087afce21593ef3299ac6767af0d3311
knight42 <anonymousknight96@gmail.com>
2018-02-15 00:37:03 +0800

    chore: update dep version && build static linking binary

--- ci/build.sh
+++ ci/build.sh
@@ -16,6 +16,7 @@ export GOOS=${GOOS:-$(go env GOOS)} GOARCH=${GOARCH:-$(go env GOARCH)}
 stage="/tmp/$NAME-$TRAVIS_TAG-$GOOS-$GOARCH/"
 rm -rf "$stage"
 
-go build -o "$stage/yukid" ./cmd/yukid
+# static linking
+CGO_ENABLED=0 go build -o "$stage/yukid" ./cmd/yukid
 cp LICENSE dist/{daemon.toml,yukid.service} "$stage"
 tar -czf "$HERE/$NAME-$TRAVIS_TAG-$GOOS-$GOARCH.tar.gz" -C /tmp -- "${stage#/tmp/}"
