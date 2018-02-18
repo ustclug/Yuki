2317595ab417f4898da19174fff87371bd0bed72
knight42 <anonymousknight96@gmail.com>
2018-02-19 02:14:49 +0800

    refact(auth): reword the error message

--- auth/middleware.go
+++ auth/middleware.go
@@ -10,7 +10,7 @@ import (
 	"github.com/labstack/echo-contrib/session"
 )
 
-var ErrMalformedCred = echo.NewHTTPError(http.StatusBadRequest, "Malformed credential")
+var ErrMalformedCred = echo.NewHTTPError(http.StatusBadRequest, "malformed credential")
 
 type Config struct {
 	Validator   func(string, string) error
@@ -26,8 +26,7 @@ func Middleware(cfg Config) echo.MiddlewareFunc {
 			if !sess.IsNew {
 				exp := sess.Values["expireAt"].(int64)
 				if !time.Unix(exp, 0).After(time.Now()) {
-					c.Response().Header().Set(echo.HeaderWWWAuthenticate, "Basic realm=Restricted")
-					return echo.NewHTTPError(http.StatusUnauthorized, "Expired cookie")
+					return echo.NewHTTPError(http.StatusUnauthorized, "expired session")
 				}
 				c.Set(userKey, sess.Values["name"])
 				return next(c)
@@ -37,13 +36,13 @@ func Middleware(cfg Config) echo.MiddlewareFunc {
 			auth := c.Request().Header.Get(echo.HeaderAuthorization)
 			parts := strings.Fields(auth)
 			if len(parts) != 2 {
-				return echo.NewHTTPError(http.StatusBadRequest, "Invalid authorization header")
+				return echo.NewHTTPError(http.StatusBadRequest, "invalid authorization header")
 			}
 			switch strings.ToLower(parts[0]) {
 			case "basic":
 				b, err := base64.StdEncoding.DecodeString(parts[1])
 				if err != nil {
-					return echo.NewHTTPError(http.StatusBadRequest, err)
+					return echo.NewHTTPError(http.StatusBadRequest, "unknown encoding")
 				}
 				cred := string(b)
 				l := strings.Split(cred, ":")
@@ -51,19 +50,19 @@ func Middleware(cfg Config) echo.MiddlewareFunc {
 					return ErrMalformedCred
 				}
 				if err = cfg.Validator(l[0], l[1]); err != nil {
-					return echo.NewHTTPError(http.StatusUnauthorized, err)
+					return echo.NewHTTPError(http.StatusUnauthorized, "incorrect username or password")
 				}
 				c.Set(userKey, l[0])
 				return next(c)
 			case "bearer":
 				err := cfg.LookupToken(parts[1])
 				if err != nil {
-					return echo.NewHTTPError(http.StatusUnauthorized, err)
+					return echo.NewHTTPError(http.StatusUnauthorized, "expired session: please login again")
 				}
 				c.Set(userKey, parts[1])
 				return next(c)
 			default:
-				return echo.NewHTTPError(http.StatusBadRequest, "Unknown scheme")
+				return echo.NewHTTPError(http.StatusBadRequest, "unknown scheme")
 			}
 		}
 	}
