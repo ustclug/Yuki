7085583c810837e25ef6474b71988e52434023ac
knight42 <anonymousknight96@gmail.com>
2020-03-09 14:06:16 +0800

    chore: update build script

--- ci/build.sh
+++ ci/build.sh
@@ -5,19 +5,19 @@ HERE=$(dirname "$0")
 cd "$HERE/../" || exit 1
 
 if [[ -z $TRAVIS_TAG ]]; then
-    TRAVIS_TAG=$(git describe --abbrev=0 --tags)
+    TRAVIS_TAG=$(git describe --tags || echo 'unknown')
 fi
 
 HERE=$(pwd)
 NAME=${NAME:-yuki}
 
-export GOOS=${GOOS:-$(go env GOOS)} GOARCH=${GOARCH:-$(go env GOARCH)}
+export GOOS=${GOOS:-$(go env GOOS)} GOARCH=${GOARCH:-$(go env GOARCH)} CGO_ENABLED=0
 
 stage="/tmp/$NAME-$TRAVIS_TAG-$GOOS-$GOARCH/"
 rm -rf "$stage"
 
 # static linking
-CGO_ENABLED=0 go build -o "$stage/yukid" ./cmd/yukid
-CGO_ENABLED=0 go build -o "$stage/yukictl" ./cmd/yukictl
+go build -trimpath -ldflags '-w -s' -o "$stage/yukid" ./cmd/yukid
+go build -trimpath -ldflags '-w -s' -o "$stage/yukictl" ./cmd/yukictl
 cp LICENSE dist/{daemon.toml,yukid.service} "$stage"
 tar -czf "$HERE/$NAME-$TRAVIS_TAG-$GOOS-$GOARCH.tar.gz" -C /tmp -- "${stage#/tmp/}"
