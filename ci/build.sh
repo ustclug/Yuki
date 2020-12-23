9e84891d096c82635a2915cd7c6dc02f3763b620
taoky <taoky1999@gmail.com>
2020-12-24 02:45:42 +0800

    chore(actions): rename TRAVIS_TAG -> GITHUB_TAG

--- ci/build.sh
+++ ci/build.sh
@@ -4,8 +4,8 @@ set -x
 HERE=$(dirname "$0")
 cd "$HERE/../" || exit 1
 
-if [[ -z $TRAVIS_TAG ]]; then
-    TRAVIS_TAG=$(git describe --tags || echo 'unknown')
+if [[ -z $GITHUB_TAG ]]; then
+    GITHUB_TAG=$(git describe --tags || echo 'unknown')
 fi
 
 HERE=$(pwd)
@@ -13,11 +13,11 @@ NAME=${NAME:-yuki}
 
 export GOOS=${GOOS:-$(go env GOOS)} GOARCH=${GOARCH:-$(go env GOARCH)} CGO_ENABLED=0
 
-stage="/tmp/$NAME-$TRAVIS_TAG-$GOOS-$GOARCH/"
+stage="/tmp/$NAME-$GITHUB_TAG-$GOOS-$GOARCH/"
 rm -rf "$stage"
 
 # static linking
 go build -trimpath -ldflags '-w -s' -o "$stage/yukid" ./cmd/yukid
 go build -trimpath -ldflags '-w -s' -o "$stage/yukictl" ./cmd/yukictl
 cp LICENSE dist/{daemon.toml,yukid.service} "$stage"
-tar -czf "$HERE/$NAME-$TRAVIS_TAG-$GOOS-$GOARCH.tar.gz" -C /tmp -- "${stage#/tmp/}"
+tar -czf "$HERE/$NAME-$GITHUB_TAG-$GOOS-$GOARCH.tar.gz" -C /tmp -- "${stage#/tmp/}"
