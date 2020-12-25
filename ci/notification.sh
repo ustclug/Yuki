090b3f0e31eeceb0146809a6f796dea1cb76d84d
taoky <taoky1999@gmail.com>
2020-12-26 02:31:13 +0800

    chore(actions): python script to handle notif webhook

--- ci/notification.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-DATA="{'repo': $GITHUB_REPOSITORY, \
-       'commit': $GITHUB_SHA, \
-       'workflow': $GITHUB_WORKFLOW \
-       'status': $ACTIONS_STATUS}"
-CURL_NETWORK="--connect-timeout 10 --max-time 10 --retry 3 --retry-delay 0 --retry-max-time 60"
-exec curl $CURL_NETWORK -sSL -D - -H "Auth: $WEBHOOK_AUTH" -d "$DATA" "$WEBHOOK_URL" -o /dev/null
